7918abacd4c4de97853e18d11fb821dec2732259
Anthony Turner <anthony@bitoblivion.com>
2015-09-11 23:19:24 -0700

    Functional test, verifying we can encrypt/decrypt successfully and the data is accessible or inaccessible depending on keyring state

---
+++ CryptInject.Tests/FunctionalTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CryptInject.Keys;
+using CryptInject.Keys.Builtin;
+using CryptInject.Tests.TestObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+namespace CryptInject.Tests
+{
+    [TestClass]
+    public class FunctionalTests
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Monitor.Enter(TestHelper.SerialExecutionLock);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Monitor.Exit(TestHelper.SerialExecutionLock);
+        }
+
+        [TestMethod]
+        public void Functional_RunsFullyCompleteObject()
+        {
+            EncryptionManager.PreloadProxyTypes();
+            
+            var keyring = new Keyring();
+            keyring.Add("AES-DES", AesEncryptionKey.Create(TripleDesEncryptionKey.Create()));
+            keyring.Add("DES", TripleDesEncryptionKey.Create());
+            keyring.Add("AES", AesEncryptionKey.Create());
+            EncryptionManager.Keyring = keyring;
+
+            // Create a new instance of the object you're serializing (class for this is below)
+            var proxy = EncryptionManager.Create<FunctionallyCompleteTestable>();
+            proxy.Populate();
+
+            EncryptionManager.Keyring.Lock();
+
+            Assert.AreEqual(proxy.String, default(string));
+            Assert.AreEqual(proxy.Integer, default(int));
+            Assert.AreEqual(proxy.Guid, default(Guid));
+            Assert.AreEqual(proxy.Guid, default(Guid));
+            Assert.AreEqual(proxy.SubObjectInstance, default(SubObject));
+
+            var bf = new BinaryFormatter() {Binder = new EncryptionProxySerializationBinder()};
+
+            var ms = new MemoryStream();
+            bf.Serialize(ms, proxy);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            EncryptionManager.Keyring.Unlock();
+
+            var replaced = (FunctionallyCompleteTestable) bf.Deserialize(ms);
+
+            var baseObject = new FunctionallyCompleteTestable();
+            baseObject.Populate();
+
+            Assert.AreEqual(replaced.Guid, baseObject.Guid);
+            Assert.AreEqual(replaced.String, baseObject.String);
+            Assert.AreEqual(replaced.Integer, baseObject.Integer);
+
+            Assert.IsNotNull(replaced.SubObjectInstance);
+
+            Assert.AreEqual(replaced.SubObjectInstance.ChildInteger, baseObject.SubObjectInstance.ChildInteger);
+        }
+    }
+}
---
+++ CryptInject.Tests/TestObjects/FunctionallyCompleteTestable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CryptInject.Tests.TestObjects
+{
+    [Serializable]
+    [SerializerRedirect(typeof(SerializableAttribute))]
+    public class FunctionallyCompleteTestable : ITestable
+    {
+        [Encryptable("AES")]
+        public virtual string String { get; set; }
+
+        [Encryptable("DES")]
+        public virtual Guid Guid { get; set; }
+
+        [SerializerRedirect(typeof(DataContractAttribute))]
+        [Encryptable("AES-DES")]
+        public virtual int Integer { get; set; }
+        
+        [Encryptable("AES")]
+        public virtual SubObject SubObjectInstance { get; set; }
+
+        public void Populate()
+        {
+            String = "I am a string!";
+            Guid = new Guid(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            Integer = 42;
+            SubObjectInstance = new SubObject()
+            {
+                ChildInteger = 64
+            };
+        }
+    }
+
+    [Serializable]
+    public class SubObject
+    {
+        public virtual int ChildInteger { get; set; }
+    }
+}
